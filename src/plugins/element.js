7ab0cffd3125b4f44a8c1aa0cd8575a9d8accadc
Millty <milkty@foxmail.com>
2022-08-19 12:01:31 +0800

    订单功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -39,6 +39,8 @@ import {
   Checkbox,
   CheckboxGroup,
   Upload,
+  Timeline,
+  TimelineItem
 } from 'element-ui'
 
 Vue.use(Button)
@@ -77,6 +79,8 @@ Vue.use(Step)
 Vue.use(Checkbox)
 Vue.use(CheckboxGroup)
 Vue.use(Upload)
+Vue.use(Timeline)
+Vue.use(TimelineItem)
 
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
